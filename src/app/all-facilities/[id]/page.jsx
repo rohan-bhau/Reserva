871c140b1042be6fc1aba929f29611af35c1f7f7
Rohan Mia <rohanmia.org@gmail.com>
2026-05-21 23:44:57 +0600

    jwt verification removed

--- src/app/all-facilities/[id]/page.jsx
+++ src/app/all-facilities/[id]/page.jsx
@@ -9,16 +9,8 @@ import { headers } from 'next/headers'
 
 const FacilityDetailPage = async ({ params }) => {
   const { id } = await params
-  const {token} = await auth.api.getToken({
-    headers: await headers()
-  })
-  console.log(token)
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/facilities/${id}`, {
-    headers: {
-      authorization: `Bearer ${token}`
-    }
-  })
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/facilities/${id}`)
   const data = await res.json()
 
   const {
