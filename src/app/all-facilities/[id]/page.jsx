a3e986442a27f45d5732e778e106c780e1a9aa77
Rohan Mia <rohanmia.org@gmail.com>
2026-05-22 09:54:53 +0600

    jwt added for details page

--- src/app/all-facilities/[id]/page.jsx
+++ src/app/all-facilities/[id]/page.jsx
@@ -9,12 +9,16 @@ import { headers } from 'next/headers'
 
 const FacilityDetailPage = async ({ params }) => {
   const { id } = await params
-  // const {token} = await auth.api.getToken({
-  //   headers: await headers()
-  // })
-  // console.log(token)
+  const {token} = await auth.api.getToken({
+    headers: await headers()
+  })
+  console.log(token)
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/facilities/${id}`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/facilities/${id}`, {
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  })
   const data = await res.json()
 
   const {
